3675160175d29b9ffa7b8fbb4d46e6a0e1334ab8
Sean-Thomo <seansthomo@gmail.com>
2026-07-25 21:48:02 +0100

    fix: update password regular expression to ensure proper validation

--- Models/DTO/RegisterDto.cs
+++ Models/DTO/RegisterDto.cs
@@ -25,7 +25,7 @@ namespace ubuntu_health_api.Models.DTO
 
     [Required(ErrorMessage = "Password is required")]
     [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 100 characters")]
-    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]",
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$",
       ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character")]
     public required string Password { get; set; }
 
